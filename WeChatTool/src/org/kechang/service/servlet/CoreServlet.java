0ca844db7b1f2f93f3c2dea823ac6eb3efebeb55
Kechang <shikechang@gwmail.gwu.edu>
2016-07-19 14:55:28 -0400

    Add files via upload

--- WeChatTool/src/org/kechang/service/servlet/CoreServlet.java
+++ WeChatTool/src/org/kechang/service/servlet/CoreServlet.java
@@ -1,7 +1,42 @@
 package org.kechang.service.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kechang.service.util.SignUtil;
 
 public class CoreServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		//Wechat encrypted signature
+		String signature = request.getParameter("signature");
+		//Timestamp
+		String timestamp = request.getParameter("timestamp");
+		//Random
+		String nonce = request.getParameter("nonce");
+		//Random string
+		String echostr = request.getParameter("echostr");
+		
+		PrintWriter out =response.getWriter();
+		//Ask for check
+		if(SignUtil.checkSignature(signature, timestamp, nonce)){
+			out.print(echostr);
+			
+		}
+		out.close();
+		out = null;
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException{
+		
+	}
 }
